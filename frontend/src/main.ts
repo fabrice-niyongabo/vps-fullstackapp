e4d5d46454991cf56ccf4b53c7c8889eab1db080
Fabrice Niyongabo <ninga.fab@gmail.com>
2024-12-11 00:55:23 +0200

    frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,6 +9,10 @@ import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { createVuetify } from "vuetify";
 
+//toast
+import ToastPlugin from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-bootstrap.css";
+
 const vuetify = createVuetify({ components, directives });
 
-createApp(App).use(router).use(vuetify).mount("#app");
+createApp(App).use(router).use(vuetify).use(ToastPlugin).mount("#app");
--- frontend/src/views/Home/Home.vue
+++ frontend/src/views/Home/Home.vue
@@ -3,7 +3,7 @@
     class="container md:w-[80%] mx-auto pt-5 pb-10 bg-gray-900 p-5 rounded-md text-white"
   >
     <div class="w-full overflow-auto">
-      <table class="text-full">
+      <table class="w-full">
         <thead>
           <tr>
             <th class="text-sm font-semibold text-left p-2">#</th>
@@ -13,10 +13,49 @@
             <th class="text-sm font-semibold text-left p-2">Action</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          <Student
+            v-for="student of students"
+            :key="student.id"
+            :student="student"
+            @fetch-students="fetchStudents"
+          />
+        </tbody>
       </table>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+import { errorHandler } from "../../utils";
+import axios from "axios";
+import { BACKEND_URL } from "../../constants";
+import Student from "./Student.vue";
+
+export interface IStudent {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+}
+
+const isLoading = ref(false);
+const students = ref<IStudent[]>([]);
+
+const fetchStudents = async () => {
+  try {
+    isLoading.value = true;
+    const res = await axios.get(BACKEND_URL + "/students");
+    students.value = res.data;
+  } catch (error) {
+    errorHandler(error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  fetchStudents();
+});
+</script>
